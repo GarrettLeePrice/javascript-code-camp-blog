5a969c3c1c73f5a0d2578608524fbe2fa8c4000a
Blake Scala and garrett price <garrett@>
2016-10-26 15:11:16 -0700

    add jumbotron styling, background image, adjust colors

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  firstFivePosts : [],
+
   model() {
     return this.store.findAll('post');
+    if (firstFivePosts.length < 5) {
+      firstFivePosts.push(this);
+    }
   }
 });
